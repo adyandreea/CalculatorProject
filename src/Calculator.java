4669edb3b3c7735a0dbab5244375eb3b0c12ba07
Andritchii Andreea <adyandreea48@gmail.com>
2025-06-25 23:50:05 +0300

    Create custom colors for calculator UI

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,21 +1,26 @@
-import java.awt.*;
-import java.awt.event.*;
-import java.util.Arrays;
-import javax.swing.*;
-import javax.swing.border.LineBorder;
+    import java.awt.*;
+    import java.awt.event.*;
+    import java.util.Arrays;
+    import javax.swing.*;
+    import javax.swing.border.LineBorder;
 
 
-public class Calculator {
-    int boardWidth = 360;
-    int boardHeight = 540;
+    public class Calculator {
+        int boardWidth = 360;
+        int boardHeight = 540;
 
-    JFrame frame = new JFrame("Calculator");
-    Calculator(){
-        frame.setVisible(true);
-        frame.setSize(boardWidth,boardHeight);
-        frame.setLocationRelativeTo(null); //Așază fereastra în centru pe ecran
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
+        Color customLightGray = new Color(212,212,210);
+        Color customDarkGray = new Color(80,80,80);
+        Color customBlack = new Color(28,28,28);
+        Color customOrange = new Color(255,149,0);
+
+        JFrame frame = new JFrame("Calculator");
+        Calculator(){
+            frame.setVisible(true);
+            frame.setSize(boardWidth,boardHeight);
+            frame.setLocationRelativeTo(null); //Așază fereastra în centru pe ecran
+            frame.setResizable(false);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setLayout(new BorderLayout());
+        }
     }
-}
