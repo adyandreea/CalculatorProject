0acf38b9335115e37e1313b3881ef390ddc2fcb4
Andritchii Andreea <adyandreea48@gmail.com>
2025-06-26 11:33:25 +0300

    Create button layout and apply color themes

--- src/Calculator.java
+++ src/Calculator.java
@@ -14,9 +14,21 @@
         Color customBlack = new Color(28,28,28);
         Color customOrange = new Color(255,149,0);
 
+        String[] buttonValues = {
+                "AC", "+/-", "%", "÷",
+                "7", "8", "9", "×",
+                "4", "5", "6", "-",
+                "1", "2", "3", "+",
+                "0", ".", "√", "="
+        };
+        String[] rightSymbols = {"÷", "×", "-", "+", "="};
+        String[] topSymbols = {"AC", "+/-", "%"};
+
         JFrame frame = new JFrame("Calculator");
         JLabel displayLabel = new JLabel();
         JPanel displayPanel = new JPanel();
+        JPanel buttonsPanel = new JPanel();
+
 
         Calculator(){
             frame.setVisible(true);
@@ -36,5 +48,32 @@
             displayPanel.setLayout(new BorderLayout());
             displayPanel.add(displayLabel);
             frame.add(displayPanel, BorderLayout.NORTH);
+
+            buttonsPanel.setLayout(new GridLayout(5,4));
+            buttonsPanel.setBackground(customBlack);
+            frame.add(buttonsPanel);
+
+            for(int i = 0; i < buttonValues.length; i++){
+                JButton button = new JButton();
+                String buttonValue = buttonValues[i];
+                button.setFont(new Font("Arial", Font.PLAIN, 30));
+                button.setText(buttonValue);
+                button.setFocusable(false);
+                button.setBorder(new LineBorder(customBlack));
+                if(Arrays.asList(topSymbols).contains(buttonValue)){
+                    button.setBackground(customLightGray);
+                    button.setForeground(customBlack);
+                }
+                else if(Arrays.asList(rightSymbols).contains(buttonValue)){
+                    button.setBackground(customOrange);
+                    button.setForeground(Color.white);
+                }
+                else{
+                    button.setBackground(customDarkGray);
+                    button.setForeground(Color.white);
+                }
+
+                buttonsPanel.add(button);
+            }
         }
     }
